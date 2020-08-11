b0f584a517ddc4aeba57069bde55c93386f781e4
BRUNOPCAVALCANTE <brunoprofetacavalcante@gmail.com>
2020-08-11 11:47:47 -0300

    Exercicios Lacos Condicionais

--- LacosCondicionais/Exercicio3.java
+++ LacosCondicionais/Exercicio3.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Exercicio3 {
 	public static void main (String [] args ) {
 		
-		Scanner ler = new Scanner(System.in);
+	 	Scanner ler = new Scanner(System.in);
 		
 		int idade ;
 		String nome;
