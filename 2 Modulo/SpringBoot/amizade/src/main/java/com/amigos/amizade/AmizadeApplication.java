95aab955f2d43a4f4c6ba9d35fc3a8f5c4866d35
bpcavalcante <brunoprofetacavalcante@gmail.com>
2020-08-31 20:33:05 -0300

    Primeiro Exercicio Sobre SpringBoot

---
+++ 2 Modulo/SpringBoot/amizade/src/main/java/com/amigos/amizade/AmizadeApplication.java
@@ -0,0 +1,13 @@
+package com.amigos.amizade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmizadeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmizadeApplication.class, args);
+	}
+
+}
---
+++ 2 Modulo/SpringBoot/amizade/src/test/java/com/amigos/amizade/AmizadeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amigos.amizade;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmizadeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
