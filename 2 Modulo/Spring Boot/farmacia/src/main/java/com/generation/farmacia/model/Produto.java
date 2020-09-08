275d22dd091c84de21a860839597850e8f5dbbfb
bpcavalcante <brunoprofetacavalcante@gmail.com>
2020-09-08 12:03:45 -0300

    Exercicios de Spring Boot

---
+++ 2 Modulo/Spring Boot/farmacia/src/main/java/com/generation/farmacia/model/Produto.java
@@ -0,0 +1,67 @@
+package com.generation.farmacia.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@Entity
+@Table(name = "TB_PRODUTO")
+public class Produto {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id_produto;
+	
+	@NotNull
+	private String nome;
+	
+	@NotNull
+	private double preco;
+	
+	@ManyToOne
+	@JsonIgnoreProperties("produto")
+	private Categoria categoria;
+
+	public long getId_produto() {
+		return id_produto;
+	}
+
+	public void setId_produto(long id_produto) {
+		this.id_produto = id_produto;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public double getPreco() {
+		return preco;
+	}
+
+	public void setPreco(double preco) {
+		this.preco = preco;
+	}
+
+	public Categoria getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(Categoria categoria) {
+		this.categoria = categoria;
+	}
+	
+	
+	
+	
+
+}
